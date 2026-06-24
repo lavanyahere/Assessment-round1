37a8b248d28805ef9596ab4281d07db02547c231
lavanyahere <lavanya1104goyal>
2026-06-24 20:40:53 +0530

    Backend completed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const bfhlRoute = require("./routes/bfhl");
 
@@ -11,7 +10,7 @@ app.use(express.json());
 
 app.use("/bfhl", bfhlRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
